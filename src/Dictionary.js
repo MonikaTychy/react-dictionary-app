5e3ed3ce73e9ca6fbe8b50349d5d2877d1361048
MonikaTychy <mo.tychy@gmail.com>
2022-07-16 20:57:28 +0200

    Integrated with dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,22 @@
 import React, { useState } from 'react';
 import './Dictionary.css';
+import axios from 'axios';
 
 export default function Dictionary () {
     let [keyword, setKeyword] = useState ("");
 
+    function showResponse(response) {
+        console.log(response.data);
+    }
+
     function handleSubmit(event) {
         event.preventDefault();
-        alert(`Searching definition for "${keyword}"`);
+        
+        // documentation https://dictionaryapi.dev/
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios.get(apiUrl).then(showResponse);
     }
-    
+
     function updateKeyword(event) {
         setKeyword(event.target.value);
     }
