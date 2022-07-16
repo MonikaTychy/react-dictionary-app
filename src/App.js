efb31c6e71a5032642b81a75c7bbeb76fb0657f4
MonikaTychy <mo.tychy@gmail.com>
2022-07-16 21:22:03 +0200

    fixed footer links

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ export default function App() {
      </main>
      <footer>
       <small>Coded by Monika Tychy, 
-        <a href='https://fantastic-macaron-9eb9ce.netlify.app' target="_blank" rel="noreferrer"> open-sourced on GitHub</a> and 
-        <a href='https://github.com/MonikaTychy/react-dictionary-app' target="_blank" rel="noreferrer"> hosted on Netlify.</a></small>
+        <a href='https://github.com/MonikaTychy/react-dictionary-app' target="_blank" rel="noreferrer"> open-sourced on GitHub</a> and 
+        <a href='https://fantastic-macaron-9eb9ce.netlify.app/' target="_blank" rel="noreferrer"> hosted on Netlify.</a></small>
      </footer>
      </div>
     </div>
